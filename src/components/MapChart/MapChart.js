dfb5006860f5cc9f3b33fc7374ebc0e88c6bfa6f
atrofimova-blackbird <atrofimova@blackbird-lab.com>
2020-12-16 11:39:14 +0200

    add reagions detalization; add zoom and pan; add hardcoded clicks heathmap

--- src/components/MapChart/MapChart.js
+++ src/components/MapChart/MapChart.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './MapChart.css';
 import { geoCentroid } from "d3-geo";
+import { scaleQuantile } from "d3-scale";
+import { csv } from "d3-fetch";
 import {
   ComposableMap,
   Geographies,
   Geography,
   Marker,
-  Annotation
+  Annotation,
+  ZoomableGroup
 } from "react-simple-maps";
 
 import allStates from "../../data/allstates";
 
-const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
-
+const geoUrlStates = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+const geoUrlCounties = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
 
 const offsets = {
   VT: [50, -8],
@@ -27,89 +30,133 @@ const offsets = {
 };
 
 const MapChart = (props) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    csv("/clicks.csv").then(counties => {
+      setData(counties);
+    });
+  }, []);
+
+  const colorScale = scaleQuantile()
+    .domain(data.map(d => d.clicks))
+    .range([
+      "#ffedea",
+      "#ffcec5",
+      "#ffad9f",
+      "#ff8a75",
+      "#ff5533",
+      "#e2492d",
+      "#be3d26",
+      "#9a311f",
+      "#782618"
+    ]);
+
   return (
     <ComposableMap className="MapChart" data-tip="" projection="geoAlbersUsa">
-      <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {geographies.map(geo => {
-              const cur = allStates.find(s => s.val === geo.id);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  stroke="#FFF"
-                  geography={geo}
-                  onMouseEnter={() => {
-                    // const NAME = geo.properties.name;
-                    const { clicks, opens } = cur;
-                    props.setTooltipContent(`Reduced analytics: ${clicks} clicks, ${opens} opens`);
-                  }}
-                  onMouseLeave={() => {
-                    props.setTooltipContent("");
-                  }}
-                  onClick={() => {
-                    props.setBeaconsContent({
-                      id: cur.id,
-                      name: geo.properties.name,
-                      opens: cur.opens,
-                      engaged_visits: cur.engaged_visits,
-                      page_views: cur.page_views,
-                      clicks: cur.clicks,
-                      ttm: cur.ttm
-                    });
-                  }}
-                  style={{
-                    default: {
-                      fill: "#D6D6DA",
-                      outline: "none",
-                      transition: "all .2s ease"
-                    },
-                    hover: {
-                      fill: "#00C6D7",
-                      outline: "none",
-                      cursor: "pointer",
-                      transition: "all .2s ease"
-                    },
-                    pressed: {
-                      fill: "#059cbb",
-                      outline: "none"
-                    }
-                  }}
-                  // fill="#DDD"
-                />
-              )
-            })}
-            {geographies.map(geo => {
-              const centroid = geoCentroid(geo);
-              const cur = allStates.find(s => s.val === geo.id);
-              return (
-                <g key={geo.rsmKey + "-name"}>
-                  {cur &&
-                  centroid[0] > -160 &&
-                  centroid[0] < -67 &&
-                  (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                    <Marker coordinates={centroid}>
-                      <text y="2" fontSize={14} textAnchor="middle">
-                        {cur.id}
-                      </text>
-                    </Marker>
-                  ) : (
-                    <Annotation
-                      subject={centroid}
-                      dx={offsets[cur.id][0]}
-                      dy={offsets[cur.id][1]}
-                    >
-                      <text x={4} fontSize={14} alignmentBaseline="middle">
-                        {cur.id}
-                      </text>
-                    </Annotation>
-                  ))}
-                </g>
-              );
-            })}
-          </>
-        )}
+      <ZoomableGroup zoom={1}>
+
+      <Geographies geography={geoUrlCounties}>
+        {({ geographies }) =>
+          geographies.map(geo => {
+            const cur = data.find(s => s.id === geo.id);
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill={cur ? colorScale(cur.clicks) : "#EEE"}
+              />
+            );
+          })
+        }
       </Geographies>
+
+      <Geographies geography={geoUrlStates}>
+        {({ geographies }) =>  {
+          // console.log("geographies", geographies);
+          return (
+            <>
+              {geographies.map(geo => {
+                const cur = allStates.find(s => s.val === geo.id);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    stroke="#FFF"
+                    geography={geo}
+                    onMouseEnter={() => {
+                      const name = geo.properties.name;
+                      const { clicks, opens } = cur;
+                      props.setTooltipContent(`${name}: ${clicks} clicks, ${opens} opens`);
+                    }}
+                    onMouseLeave={() => {
+                      props.setTooltipContent("");
+                    }}
+                    onClick={() => {
+                      props.setBeaconsContent({
+                        id: cur.id,
+                        name: geo.properties.name,
+                        opens: cur.opens,
+                        engaged_visits: cur.engaged_visits,
+                        page_views: cur.page_views,
+                        clicks: cur.clicks,
+                        ttm: cur.ttm
+                      });
+                    }}
+                    style={{
+                      default: {
+                        fill: "transparent",
+                        outline: "none",
+                        transition: "all .2s ease"
+                      },
+                      hover: {
+                        fill: "rgba(255, 255, 255, .6)",
+                        outline: "none",
+                        cursor: "pointer",
+                        transition: "all .2s ease"
+                      },
+                      pressed: {
+                        fill: "rgba(255, 225, 225, .8)",
+                        outline: "none"
+                      }
+                    }}
+                    // fill="#DDD"
+                  />
+                )
+              })}
+              {/*{geographies.map(geo => {*/}
+              {/*const centroid = geoCentroid(geo);*/}
+              {/*const cur = allStates.find(s => s.val === geo.id);*/}
+              {/*return (*/}
+              {/*<g key={geo.rsmKey + "-name"}>*/}
+              {/*{cur &&*/}
+              {/*centroid[0] > -160 &&*/}
+              {/*centroid[0] < -67 &&*/}
+              {/*(Object.keys(offsets).indexOf(cur.id) === -1 ? (*/}
+              {/*<Marker coordinates={centroid}>*/}
+              {/*<text y="2" fontSize={14} textAnchor="middle">*/}
+              {/*{cur.id}*/}
+              {/*</text>*/}
+              {/*</Marker>*/}
+              {/*) : (*/}
+              {/*<Annotation*/}
+              {/*subject={centroid}*/}
+              {/*dx={offsets[cur.id][0]}*/}
+              {/*dy={offsets[cur.id][1]}*/}
+              {/*>*/}
+              {/*<text x={4} fontSize={14} alignmentBaseline="middle">*/}
+              {/*{cur.id}*/}
+              {/*</text>*/}
+              {/*</Annotation>*/}
+              {/*))}*/}
+              {/*</g>*/}
+              {/*);*/}
+              {/*})}*/}
+            </>
+          )
+        }}
+      </Geographies>
+
+      </ZoomableGroup>
     </ComposableMap>
   );
 };
--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -6,15 +6,15 @@ const ResultCard = (props) => {
     <div className="ResultCard">
       <div className="header">
         <h2>Full analytics</h2>
-        <p>State: <span>{props.beacons.name || "Default"} ({props.beacons.id || "Default"})</span></p>
+        <p>State: <span>{props.beacons.name || "Not selcted"}</span></p>
         <p>FSA: 70578</p>
       </div>
       <div className="beacons-data">
-        <p>opens: <span>{props.beacons.opens || 0}</span></p>
-        <p>engaged_visits: <span>{props.beacons.engaged_visits || 0}</span></p>
-        <p>page_views: <span>{props.beacons.page_views || 0}</span></p>
-        <p>clicks: <span>{props.beacons.clicks || 0}</span></p>
-        <p>ttm: <span>{props.beacons.ttm || 0}</span></p>
+        <p>opens: <span>{props.beacons.opens || null}</span></p>
+        <p>engaged_visits: <span>{props.beacons.engaged_visits || null}</span></p>
+        <p>page_views: <span>{props.beacons.page_views || null}</span></p>
+        <p>clicks: <span>{props.beacons.clicks || null}</span></p>
+        <p>ttm: <span>{props.beacons.ttm || null}</span></p>
       </div>
     </div>
   );
